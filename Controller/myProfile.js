674afd8f4ac28cea67b902ec36596dcfb88e5f2b
Joakim Larsen <joakim@storasset.dk>
2020-12-03 14:57:25 +0100

    Extra API

--- Controller/myProfile.js
+++ Controller/myProfile.js
@@ -58,3 +58,22 @@ function deleteUser(){
          xhttp.send();
      });
    });
+
+
+/**Et get request sendes som forbinder serveren med siden når bruger opretter sig. 
+   * Efter der er kørt node.server.js i consolen så kan localhost:3000 skrives i postman eller føges vi linket*/
+  document.addEventListener("DOMContentLoaded", function() {
+
+    let apiButton = document.getElementById("deleteBtn");
+     apiButton.addEventListener("click", function(){
+         var xhttp = new XMLHttpRequest();
+         xhttp.onreadystatechange = function() {
+             if (this.readyState == 4 && this.status == 200) {
+                 // Typical action to be performed when the document is ready:
+                 alert(xhttp.responseText);
+             }
+         };
+         xhttp.open("GET", "http://localhost:3002", true);
+         xhttp.send();
+     });
+   });
